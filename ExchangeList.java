35d0a0b98d4a73155dabe36fd700e4ff33ff2070
vsd550 <vijayshankerdubey550@gmail.com>
2018-06-23 22:43:01 +0530

    removing some files

--- ExchangeList.java
+++
@@ -1,4 +0,0 @@
-
-public class ExchangeList extends LinkedList<Exchange> {
-	LinkedList<Exchange> xl = new LinkedList<Exchange>();
-}
--- MobilePhoneSet.java
+++
@@ -1,5 +0,0 @@
-
-public class MobilePhoneSet {
-	Myset<MobilePhone> swaroop = new Myset<MobilePhone>();
-
-}
